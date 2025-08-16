0b6ce53556318c5345c96fb2ea4f24f56a1f91b0
precious thomas <preshusthomas28@gmail.com>
2025-08-16 20:51:52 +0100

    alx book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 SELECT DATABASE();
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(150) NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
